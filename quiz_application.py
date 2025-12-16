dccf92a6b05e78a651d0da2a1d4592af68538479
PioterDev <pimikotr@gmail.com>
2025-12-16 16:25:00 +0100

    Fantasy/Horror

--- quiz_application.py
+++ quiz_application.py
@@ -1,6 +1,10 @@
+import sys
+sys.path.append("lib")
+
 import tkinter
 import clips
 from functools import partial
+
 rules_filepath = "./rules.clp"
 question = ""
 curr_fact = ""
@@ -30,22 +34,19 @@ title.grid(row=0,column=0)
 #button function
 def button_press(answer = ''):
     global question, curr_fact, possible_answers, question_id, buttons
-    
+
     #czyszczenie przyciskow
     for i in range(len(buttons)):
         buttons[i].destroy()
     buttons = []
-    
+
     if(answer != ''):
         print("asserting:",'('+curr_fact+' '+answer+')')
         clips_env.assert_string('('+curr_fact+' '+answer+')')
-    
+
     clips_env.run()
     for i in clips_env.facts():
-        fact_formatted = str(i)
-        fact_formatted = fact_formatted.replace('(','')
-        fact_formatted = fact_formatted.replace(')','')
-        fact_formatted = fact_formatted.split(' ')
+        fact_formatted = str(i).replace("(", "").replace(")", "").split(" ")
         if fact_formatted[0] == "question":
             question = fact_formatted[1].replace('_',' ')
             curr_fact = fact_formatted[2]
@@ -67,7 +68,7 @@ def button_press(answer = ''):
     # input = text_input.get("1.0",'end-1c')
     # print(input)
     # clips_env.assert_string(input)
-    
+
 
 button = tkinter.Button(app,text="START",width=25,command=button_press)
 button.grid(row=3,column=0)
